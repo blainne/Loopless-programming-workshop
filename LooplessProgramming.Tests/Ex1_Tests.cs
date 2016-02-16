245eb238b47b6ab8add68dc5f3d3a3472a40c302
blainne <blainne@wp.pl>
2016-02-17 00:28:56 +0100

    intial early version

---
+++ LooplessProgramming.Tests/Ex1_Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LooplessProgramming.Tests
+{
+    [TestClass]
+    public class Ex1_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ LooplessProgramming/Ex1_LoopFiltering.cs
@@ -0,0 +1,17 @@
+﻿using LooplessProgramming.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooplessProgramming
+{
+    public class Step1_LoopFiltering
+    {
+        public IEnumerable<SpaceWarship> GetShipsWithYoungCaptains()
+        {
+            return null; // TODO start with testss
+        }
+    }
+}
