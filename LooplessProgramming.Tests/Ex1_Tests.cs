18379ec35b438b012b20605279cf0f39e1028c1e
blainne <grzegorz.slawecki@gmail.com>
2016-02-17 21:45:14 +0100

    First excersise and first test.

--- LooplessProgramming.Tests/Ex1_Tests.cs
+++ LooplessProgramming.Tests/Ex1_Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LooplessProgramming.Tests
@@ -6,9 +7,17 @@ namespace LooplessProgramming.Tests
     [TestClass]
     public class Ex1_Tests
     {
+        Ex1_LoopFiltering Ex1 = new Ex1_LoopFiltering();
+        SampleData data = new SampleData();
+
         [TestMethod]
-        public void TestMethod1()
+        public void AllCaptainsWithExperienceNoGreaterThan25()
         {
+            var result = Ex1.GetShipsWithYoungCaptains();
+
+            result
+                .Should().OnlyContain(ship => ship.Captain.YearsOfService <= 25)
+                .And.OnlyHaveUniqueItems();
         }
     }
 }
--- LooplessProgramming/Ex1_LoopFiltering.cs
+++ LooplessProgramming/Ex1_LoopFiltering.cs
@@ -7,11 +7,25 @@ using System.Threading.Tasks;
 
 namespace LooplessProgramming
 {
-    public class Step1_LoopFiltering
+    public class Ex1_LoopFiltering
     {
+        private SampleData data = new SampleData();
         public IEnumerable<SpaceWarship> GetShipsWithYoungCaptains()
         {
-            return null; // TODO start with testss
+            //This function should return a collection of ships
+            //that are commanded by captains with service 
+            //no longer than 25 years.
+            //Use a loop to implement it.
+
+            var result = new List<SpaceWarship>();
+            foreach (var ship in data.Ships)
+            {
+                if(ship.Captain.YearsOfService <= 25)
+                    result.Add(ship);
+            }
+
+            return result;
+
         }
     }
 }
