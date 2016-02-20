c618c5e070dda1b8a99bceb47d86e2bdd82b146b
blainne <blainne@wp.pl>
2016-02-20 19:30:46 +0100

    ex4 completed

---
+++ LooplessProgramming.Tests/Ex4_Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using LooplessProgramming.Model;
+using System.Linq;
+
+namespace LooplessProgramming.Tests
+{
+    [TestClass]
+    public class Ex4_Tests
+    {
+        private readonly Ex4_FilterAsExtension.Ex4_FilteringData Ex4 = 
+                            new Ex4_FilterAsExtension.Ex4_FilteringData();
+
+        private readonly SampleData data = new SampleData();
+        private readonly SampleData originalData = new SampleData();
+
+        [TestMethod]
+        public void GetAllBattleshipsReturnsNoDuplicates()
+        {
+            var result = Ex4.GetAllBattleships(data);
+
+            result.Should().OnlyHaveUniqueItems();
+        }
+
+
+        [TestMethod]
+        public void GetAllBattleshipsReturnsOnlyBattleships()
+        {
+            var result = Ex4.GetAllBattleships(data);
+            result.Should().OnlyContain(ship => ship.ShipClass == WarshipClass.Battleship);
+        }
+
+        [TestMethod]
+        public void GetAllBattleshipsReturnsAllBattleships()
+        {
+            var result = Ex4.GetAllBattleships(data);
+            result.Should().HaveCount(2);
+        }
+
+
+        [TestMethod]
+        public void GetAllCommandersOnMReturnsNoDuplicates()
+        {
+            var result = Ex4.GetCommandersOnM(data);
+
+            result.Should().OnlyHaveUniqueItems();
+        }
+
+        [TestMethod]
+        public void GetAllCommandersOnMReturnsOnlyCommandersOnM()
+        {
+            var result = Ex4.GetCommandersOnM(data);
+
+            result.Should().OnlyContain(person => person.Name.ToLower().StartsWith("m"));
+        }
+
+        [TestMethod]
+        public void GetAllCommandersOnMReturnsAllCommandersOnM()
+        {
+            var result = Ex4.GetCommandersOnM(data);
+
+            result.Should().HaveCount(3);
+        }
+    }
+}
--- LooplessProgramming/Ex4_FilterAsExtension.cs
+++ LooplessProgramming/Ex4_FilterAsExtension.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LooplessProgramming.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,38 @@ namespace LooplessProgramming
 {
     public static class Ex4_FilterAsExtension
     {
-        //Your filtering method (the one that contains for loop) shall be now pretty much done.
+        //Your filtering method (the one that contains for loop) shall be pretty much done.
         //Now, take it and turn it into an extension method,
         //so it can be used in a more convenient and readable way
 
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> collection, Func<T, bool> condition)
+        {
+            var result = new List<T>();
+            var enumerator = collection.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                var elem = enumerator.Current;
+                if (condition(elem))
+                    result.Add(elem);
+            }
+            return result;
+        }
 
+
+        //Use newly created to implement our data processing methods in the class below
+        public class Ex4_FilteringData
+        {
+            public IEnumerable<SpaceWarship> GetAllBattleships(SampleData data)
+            {
+                //This method, as it's name implies, shall simply return all battleships.
+                return data.Ships.Filter(ship => ship.ShipClass == WarshipClass.Battleship);
+            }
+            
+            public IEnumerable<Person> GetCommandersOnM(SampleData data)
+            {
+                //Return all commanders whose names start with letter M or m
+                return data.Commanders.Filter(person => person.Name.ToLower().StartsWith("m"));
+            }
+        }
     }
 }
