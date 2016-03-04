c7718e88870b5d7ac7b62a0272bbe2450843fde0
blainne <grzegorz.slawecki@gmail.com>
2016-03-04 23:17:27 +0100

    finished reduce exercise 2

--- LooplessProgramming.Tests/Reducing/ReducingEx2_Tests.cs
+++ LooplessProgramming.Tests/Reducing/ReducingEx2_Tests.cs
@@ -5,14 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using LooplessProgramming.Reducing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
 
 namespace LooplessProgramming.Tests.Reducing
 {
     [TestClass]
     public class ReducingEx2_Tests
     {
-        private readonly Ex2_ExtractedLoop Ex2 = new Ex2_ExtractedLoop();
+        private readonly Ex2_ExtractedGenericLoop Ex2 = new Ex2_ExtractedGenericLoop();
 
         private readonly SampleData data = new SampleData();
+
+        [TestMethod]
+        public void GetSmallestShipReturnsCrewWithLeastCrew()
+        {
+            var expected = data.Ships.OrderBy(s => s.Crew).First();
+
+            var result = Ex2.GetSmallestShip(data);
+
+            result.Should().Be(expected);
+        }
+
+        [TestMethod]
+        public void GetCommaSeparatedCommandersNamesReturnsCorrectValue()
+        {
+            var expected = 
+                data.Commanders
+                    .Aggregate("", (acc, comm) => acc + comm.Name + ",");
+
+            var result = Ex2.GetCommaSeparatedCommandersNames(data);
+
+            result.Should().Be(expected);
+        }
     }
 }
